148f2b3025fbd1705d9176084b511258758853e8
Mikołaj Gramowski <gramosmiki@onet.pl>
2025-06-24 13:22:14 +0200

    assets: add SVG graphics and components
    - Add vector graphics for arrows, borders, and logos
    - Add React SVG components for interactive graphics
    - Add brand logo and design elements
    - Add decorative lines and UI elements

---
+++ public/svg/Corner.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Corner = () => {
+  return (
+    <svg
+      width="29"
+      height="29"
+      viewBox="0 0 29 27"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M29 26L8.5 26L1 16.7917L1 -9.53674e-07"
+        stroke="white"
+        strokeOpacity="0.33"
+      />
+    </svg>
+  );
+};
+
+export default Corner;
---
+++ public/svg/Theline.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Theline = () => {
+  return (
+<svg width="9" height="76" viewBox="0 0 9 76" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1 -3.05977e-07L1 61.25L8 75" stroke="white" strokeOpacity="0.33"/>
+</svg>
+
+  )
+}
+
+export default Theline
